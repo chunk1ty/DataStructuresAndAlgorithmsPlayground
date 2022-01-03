f9b7c52b9a779882b269feb6e7412aa06485b375
chunk1ty <andriyan.krastev@gmail.com>
2022-01-03 09:23:36 +0200

    added unit tests for selection sort

---
+++ Algorithms.Tests/Sorting/SelectionSorterTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Algorithms.Sorting;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Tests.Sorting
+{
+    [TestClass]
+    public class SelectionSorterTests
+    {
+        [TestMethod]
+        public void Sort_WithRandomNumbers_ShouldSortCollection()
+        {
+            // Arrange
+            var selectionSorter = new SelectionSorter();
+
+            // Act
+            var collection = new List<int>() { 4, 5, 3, 8, 2, 6, 10, 7, 9, 1 };
+            selectionSorter.Sort(collection);
+
+            // Act
+            collection.Count.Should().Be(10);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                collection[i - 1].Should().Be(i);
+            }
+        }
+
+        [TestMethod]
+        public void Sort_WithOneNumber_ShouldSortCollection()
+        {
+            // Arrange
+            var selectionSorter = new SelectionSorter();
+
+            // Act
+            var collection = new List<int>() { 4 };
+            selectionSorter.Sort(collection);
+
+            // Act
+            collection.Count.Should().Be(1);
+            collection.First().Should().Be(4);
+        }
+    }
+}
