7d1401b42f3a39fc1c5af620c637bc1873eb0b20
chunk1ty <andriyan.krastev@gmail.com>
2022-01-03 09:04:07 +0200

    added bubble sort tests

---
+++ Algorithms.Tests/Sorting/BubbleSorterTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Algorithms.Sorting;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Tests.Sorting
+{
+    [TestClass]
+    public class BubbleSorterTests
+    {
+        [TestMethod]
+        public void Sort_WithRandomNumbers_ShouldSortCollection()
+        {
+            // Arrange
+            var bubbleSorter = new BubbleSorter();
+
+            // Act
+            var collection = new List<int>() { 4, 5, 3, 8, 2, 6, 10, 7, 9, 1 };
+            bubbleSorter.Sort(collection);
+
+            // Act
+            collection.Count.Should().Be(10);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                collection[i - 1].Should().Be(i);
+            }
+        }
+
+        [TestMethod]
+        public void Sort_WithOneNumber_ShouldSortCollection()
+        {
+            // Arrange
+            var bubbleSorter = new BubbleSorter();
+
+            // Act
+            var collection = new List<int>() { 4 };
+            bubbleSorter.Sort(collection);
+
+            // Act
+            collection.Count.Should().Be(1);
+            collection.First().Should().Be(4);
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -14,7 +14,7 @@ namespace Algorithms
 
             // CombinatoricPlaygrounder.Play();
 
-            SortingPlaygrounder.Play();
+            // SortingPlaygrounder.Play();
         }
     }
 }
